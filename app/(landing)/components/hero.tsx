0d0c270455457b2c3b178b21ebedd02511f89478
Harsh <74731135+harshturna@users.noreply.github.com>
2025-01-15 17:42:27 -0600

    Update hero title line height

--- app/(landing)/components/hero.tsx
+++ app/(landing)/components/hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
               Beyond Simple
             </h3>
             <br />
-            <h1 className="text-gradient text-5xl md:text-7xl">
+            <h1 className="text-gradient text-5xl md:text-7xl !leading-[95px]">
               Web Analytics
             </h1>
           </div>
